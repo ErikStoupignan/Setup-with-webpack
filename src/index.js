737ed0edd8f59385bfa1105e6621ef5ded89e897
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-09 11:58:06 -0500

    Add setup and html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack Im Erik Stou'], ' ');
 
   return element;
 }
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  plugins: [
+  new HtmlWebpackPlugin({
+    title: 'Setup-with-Webpack',
+    }),
+  ],  
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
