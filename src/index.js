279670a525fc24cfbe15d979b8741eda363b46ec
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-09 12:55:22 -0500

    Add setup img on css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack Im Erik Stou'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  element.classList.add('img');
+
+
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: './src/index.js',
-  plugins: [
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-    }),
-  ],  
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      }),
+    ],  
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
